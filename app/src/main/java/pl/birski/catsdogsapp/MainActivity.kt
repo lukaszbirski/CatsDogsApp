a44f306b5bfa2c4f41f54ce09e6b3e824dc0ae84
Lukasz <lukasz.birski@gmail.com>
2022-10-09 13:34:39 +0200

    Added classifier

--- app/src/main/java/pl/birski/catsdogsapp/MainActivity.kt
+++ app/src/main/java/pl/birski/catsdogsapp/MainActivity.kt
@@ -1,16 +1,49 @@
 package pl.birski.catsdogsapp
 
+import android.graphics.drawable.BitmapDrawable
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import pl.birski.catsdogsapp.databinding.ActivityMainBinding
+import java.util.Locale
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     private lateinit var binding: ActivityMainBinding
+
+    private val mInputSize = 224
+    private val mModelPath = "converted_model.tflite"
+    private val mLabelPath = "label.txt"
+
+    private lateinit var classifier: Classifier
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
+
+        initView()
+
+        classifier = Classifier(assets, mModelPath, mLabelPath, mInputSize)
+
         setContentView(binding.root)
+    }
+
+    private fun initView() {
+        binding.iv1.setOnClickListener(this)
+        binding.iv2.setOnClickListener(this)
+        binding.iv3.setOnClickListener(this)
+        binding.iv4.setOnClickListener(this)
+    }
+
+    override fun onClick(view: View) {
+        val bitmap = ((view as ImageView).drawable as BitmapDrawable).bitmap
+        val result = classifier.recognizeImage(bitmap)
+        Toast.makeText(
+            this,
+            "Recognized: ${result[0].title.uppercase(Locale.ROOT)}",
+            Toast.LENGTH_SHORT
+        ).show()
     }
 }
---
+++ app/src/main/java/pl/birski/catsdogsapp/Recognition.kt
@@ -0,0 +1,7 @@
+package pl.birski.catsdogsapp
+
+data class Recognition(
+    var id: String = "",
+    var title: String = "",
+    var confidence: Float = 0F
+)
